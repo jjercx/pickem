b89348aeb57fe80839e2efbe73638cd537c697e8
Juan José Ramírez <jjramirez@pucp.pe>
2021-02-02 10:43:51 -0500

    Annotate models

--- backend/app/models/player_pick.rb
+++ backend/app/models/player_pick.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: player_picks
+#
+#  id              :bigint           not null, primary key
+#  pick_id         :bigint           not null
+#  player_id       :bigint           not null
+#  selected_option :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class PlayerPick < ApplicationRecord
   belongs_to :pick
   belongs_to :player
--- backend/test/models/player_pick_test.rb
+++ backend/test/models/player_pick_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: player_picks
+#
+#  id              :bigint           not null, primary key
+#  pick_id         :bigint           not null
+#  player_id       :bigint           not null
+#  selected_option :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require "test_helper"
 
 class PlayerPickTest < ActiveSupport::TestCase
