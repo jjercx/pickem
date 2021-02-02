9d38efc6e46c03056f944b670b8cf2c8d06ffa3e
Juan José Ramírez <jjramirez@pucp.pe>
2021-02-01 21:48:03 -0500

    Add teams

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_015847) do
+ActiveRecord::Schema.define(version: 2021_02_02_023040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 2021_02_02_015847) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
