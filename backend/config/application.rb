22893be6e645028347b10278f09ad0c391a68af1
Juan José Ramírez <jjramirez@pucp.pe>
2021-02-02 22:44:22 -0500

    Add serializers for JSONAPI

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,6 +1,8 @@
-require_relative "boot"
+# frozen_string_literal: true
 
-require "rails/all"
+require_relative 'boot'
+
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +12,8 @@ module Pickem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.autoload_paths << Rails.root.join('app/serializers')
 
     # Configuration for the application, engines, and railties goes here.
     #
