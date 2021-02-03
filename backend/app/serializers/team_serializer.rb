7a2f6100cdbbf6ead5f2ac417c04c398f847ef50
Juan José Ramírez <jjramirez@pucp.pe>
2021-02-02 23:33:25 -0500

    Annotate models

--- backend/app/serializers/team_serializer.rb
+++ backend/app/serializers/team_serializer.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: teams
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class TeamSerializer
   include JSONAPI::Serializer
   attribute :name
